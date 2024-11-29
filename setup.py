f949ee2a358549008b3325b61de3ff04e990f67d
apurvad-wa <apurva.daksh@thoughtworks.com>
2024-11-29 13:08:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
+import os
+
+def include_all_files(directory):
+    file_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_list.append(os.path.relpath(os.path.join(root, file), directory))
+    return file_list
 
 setup(
     name="saprot",
     version="0.0.1",
+    license='MIT',
+    url='https://github.com/apurvad-wa/SaProt',
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "saprot": include_all_files("."), 
+    }
 )
